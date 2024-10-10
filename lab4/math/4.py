3f1f33f23a2ba4bd96b6e8cd4be9d827f0a52218
aigeriri <a_zhaisanova@kbtu.kz>
2024-10-10 12:29:17 +0500

    Add files via upload

---
+++ lab4/math/4.py
@@ -0,0 +1,6 @@
+#Write a Python program to calculate the area of a parallelogram.
+import math
+base = int(input("Enter the length of base: "))
+height = int(input("Enter the height of parallelogram: "))
+area = base * height
+print("Area equals to:", float(area))
